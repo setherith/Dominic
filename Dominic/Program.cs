1e666bab495a16c7ed7a5213a07b526e6af6cdc5
Shane Pudner <setherith@gmail.com>
2020-06-14 07:05:40 +0100

    Getting controller data with the use of a SharpDX

---
+++ Dominic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using SharpDX.XInput;
+
+namespace Dominic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var controller = new Controller(UserIndex.Two);
+
+            Console.WriteLine(controller.IsConnected);
+            Console.WriteLine(controller.UserIndex);
+
+            //controller.SetVibration(new Vibration()
+            //{
+            //    LeftMotorSpeed = 35000,
+            //    RightMotorSpeed = 35000
+            //});
+
+            var batt = controller.GetBatteryInformation(BatteryDeviceType.Gamepad);
+            Console.WriteLine(batt.BatteryLevel);
+            Console.WriteLine(batt.BatteryType);
+
+            while (true)
+            {
+                var state = controller.GetState();
+                Console.WriteLine(state.Gamepad.Buttons);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
